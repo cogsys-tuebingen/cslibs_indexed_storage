1639bdd3a7d21d6a452ce095a602709557cde8c1
Daejiv <daejiv@googlemail.com>
2016-12-18 20:26:19 +0100

    prepare for non owning storage (aka pointer)

--- include/cslibs_clustering/interface/data/data.hpp
+++ include/cslibs_clustering/interface/data/data.hpp
@@ -9,33 +9,63 @@ namespace interface
 {
 namespace detail
 {
-template<option::MergeStrategy strategy>
+template<typename type, option::MergeStrategy strategy>
 struct data_merger;
 
-template<>
-struct data_merger<option::MergeStrategy::REPLACE>
+template<typename type>
+struct data_merger<type, option::MergeStrategy::REPLACE>
 {
-    template<typename type, typename... Args>
-    static constexpr inline auto apply(type& self, Args&&... args) -> typename std::enable_if<std::is_copy_assignable<type>::value>::type
+    template<typename... Args>
+    static constexpr inline void apply(type& self, Args&&... args)
+    {
+        do_apply(typename std::is_copy_assignable<type>::type{}, self, std::forward<Args>(args)...);
+    }
+
+private:
+    template<typename... Args>
+    static constexpr inline void do_apply(std::true_type, type& self, Args&&... args)
     {
         self = {std::forward<Args>(args)...};
     }
 
-    template<typename type, typename... Args>
-    static constexpr inline auto apply(type& self, Args&&... args) -> typename std::enable_if<!std::is_copy_assignable<type>::value>::type
+    template<typename... Args>
+    static constexpr inline void do_apply(std::false_type, type& self, Args&&... args)
     {
         self.~type();
         new (&self) type(std::forward<Args>(args)...);
     }
 };
 
-template<>
-struct data_merger<option::MergeStrategy::MERGE>
+template<typename type>
+struct data_merger<type, option::MergeStrategy::MERGE>
 {
-    template<typename type, typename... Args>
+    template<typename... Args>
     static constexpr inline void apply(type& self, Args&&... args)
     {
         self.merge(std::forward<Args>(args)...);
+    }
+};
+
+
+template<typename base_type>
+struct data_merger<base_type*, option::MergeStrategy::REPLACE>
+{
+    using type = typename std::add_pointer<base_type>::type;
+
+    static constexpr inline void apply(type*& self, type other)
+    {
+        self = other;
+    }
+};
+
+template<typename base_type>
+struct data_merger<base_type*, option::MergeStrategy::MERGE>
+{
+    using type = typename std::add_pointer<base_type>::type;
+
+    static constexpr inline void apply(type& self, type other)
+    {
+        self->merge(*other);
     }
 };
 
@@ -55,7 +85,24 @@ struct data_interface
     template<option::MergeStrategy strategy, typename... Args>
     static inline constexpr void merge(type& self, Args&&... args)
     {
-        return detail::data_merger<strategy>::apply(self, std::forward<Args>(args)...);
+        return detail::data_merger<type, strategy>::apply(self, std::forward<Args>(args)...);
+    }
+};
+
+template<typename data_t>
+struct data_interface<data_t*>
+{
+    using type = data_t*;
+
+    static inline constexpr type create(type self)
+    {
+        return self;
+    }
+
+    template<option::MergeStrategy strategy>
+    static inline constexpr void merge(type& self, type other)
+    {
+        return detail::data_merger<type, strategy>::apply(self, other);
     }
 };
 
