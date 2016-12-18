93b45ffc9b2283a2ce3229f0550153630d1704e7
Daejiv <daejiv@googlemail.com>
2016-12-18 17:23:16 +0100

    example: cluster benchmark

--- example/benchmark/benchmark.cpp
+++ example/benchmark/benchmark.cpp
@@ -6,12 +6,17 @@
 #include <cslibs_clustering/backend/array/array.hpp>
 #include <cslibs_clustering/backend/simple/unordered_component_map.hpp>
 #include <cslibs_clustering/interface/index/index_std.hpp>
+#include <cslibs_clustering/operations/clustering.hpp>
+
+#include <map>
+
 namespace cc = cslibs_clustering;
 
 namespace tests
 {
     struct Data
     {
+        int cluster = -1;
         std::vector<const data::Point*> samples;
         double weight;
 
@@ -38,6 +43,51 @@ namespace tests
                  static_cast<int>(std::floor(point.z / bin_sizes[2])) };
     }
 
+    struct ClusterOp
+    {
+        //! called when a new cluster should be started
+        bool start(const Index&, Data& data)
+        {
+            if (data.cluster != -1)
+                return false;
+
+            current_cluster +=1;
+            clusters.emplace(current_cluster, data.samples);
+
+            data.cluster = current_cluster;
+
+            return true;
+        }
+
+        //! called when a cluster is extended due to found neighbors
+        bool extend(const Index&, const Index&, Data& data)
+        {
+            if (data.cluster != -1)
+                return false;
+
+            auto& cluster = clusters[current_cluster];
+            cluster.insert(cluster.end(), data.samples.begin(), data.samples.end());
+
+            data.cluster = current_cluster;
+            return true;
+        }
+
+        //! used neighborhood, look at direct neighbors only
+        using neighborhood_t = cc::operations::clustering::GridNeighborhoodStatic<std::tuple_size<Index>::value, 3>;
+        using visitor_index_t = neighborhood_t::offset_t;   //!< currently needed by the clustering API
+
+        //! vistor implementation for neighbors
+        template<typename visitor_t>
+        void visit_neighbours(const Index&, const visitor_t& visitior)
+        {
+            static constexpr auto neighborhood = neighborhood_t{};
+            neighborhood.visit(visitior);
+        }
+
+        int current_cluster = -1;   //!< keep track of the current cluster index
+        std::unordered_map<int, std::vector<const data::Point*>> clusters;
+    };
+
 
     template<typename Storage>
     void create(const data::Points& samples)
@@ -48,6 +98,20 @@ namespace tests
         {
             storage.insert(create_index(sample), create_data(sample));
         }
+    }
+
+    template<typename Storage>
+    void cluster(const data::Points& samples)
+    {
+        Storage storage;
+
+        for (const data::Point& sample : samples)
+            storage.insert(create_index(sample), create_data(sample));
+
+        ClusterOp clusters;
+        cc::operations::clustering::Clustering<Storage> clusterer(storage);
+        clusterer.cluster(clusters);
+//        printf("%lu", clusters.clusters.size());
     }
 }
 
@@ -82,7 +146,13 @@ int main(int argc, char* argv[])
         cc::backend::simple::UnorderedComponentMap
     >;
 
-    timing::Benchmark::timing<500>("kd-tree", std::bind(&tests::create<kd_tree>, std::cref(points)));
-    timing::Benchmark::timing<500>("array  ", std::bind(&tests::create<array>, std::cref(points)));
-    timing::Benchmark::timing<500>("paged  ", std::bind(&tests::create<paged>, std::cref(points)));
+    std::cout << "Create:" << std::endl;
+    timing::Benchmark::timing<500>("\tkd-tree", std::bind(&tests::create<kd_tree>, std::cref(points)));
+    timing::Benchmark::timing<500>("\tarray  ", std::bind(&tests::create<array>, std::cref(points)));
+    timing::Benchmark::timing<500>("\tpaged  ", std::bind(&tests::create<paged>, std::cref(points)));
+
+    std::cout << "Create & Cluster:" << std::endl;
+    timing::Benchmark::timing<500>("\tkd-tree", std::bind(&tests::cluster<kd_tree>, std::cref(points)));
+    timing::Benchmark::timing<500>("\tarray  ", std::bind(&tests::cluster<array>, std::cref(points)));
+    timing::Benchmark::timing<500>("\tpaged  ", std::bind(&tests::cluster<paged>, std::cref(points)));
 }
